d62dcdc020987dbf7fc333eb5680471366360c9f
Patrick Andre <patrickandre26@gmail.com>
2017-01-29 19:19:01 -0500

    expose env

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', env: JSON.stringify(process.env) });
 });
 
 module.exports = router;
