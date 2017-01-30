74c9a0827b215ce2e70e3eb4f47912dd292dc8ad
Patrick Andre <patrickandre26@gmail.com>
2017-01-29 21:13:09 -0500

    add env setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', env: JSON.stringify(process.env) });
+  res.render('index', { title: 'Express', env: JSON.stringify(process.env.NODE_ENV) });
 });
 
 module.exports = router;
